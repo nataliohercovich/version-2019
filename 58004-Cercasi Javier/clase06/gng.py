8ba794f209da46e440885f625f09d2a4437b67b7
javicercasi <jacercasii@gmail.com>
2019-05-14 11:20:02 -0300

    comit local

--- 58004-Cercasi Javier/clase06/gng.py
+++ 58004-Cercasi Javier/clase06/gng.py
@@ -1,2 +1,47 @@
-class HumanAgainstComputerGame(object):
+class Game():
+    def __init__(self):
+        self.isplaying=True
+    def finish(self):
+        self.playing=False
+
+class HumanAgainstComputerGame(Game):       #como tambien es un juego, puedo llamar a la clase Game (herencia)
+
+    def __init__(self):
+        '''self.is_playing=True #nos ahorramos esto con la clase game'''
+
+        super()__init__():      #aca llama al init de la clase Game, es como la general
+        self.secret_number= random.randrange(101)       #self es un objeto,para reutilizarlo
+
+
+    def pensar_numero(self,number):
+        if self.secret_number> number:
+            return "My number is bigger"
+
+        if self.secret_number< number:
+            return "My number is smaller"
+
+        if self.secret_number == number:
+            return "You win"
+
+class ComputerAgainstHumanGame(object):
+
+    def __init__(self):
+        '''self.isplaying=True'''
+        self.cota_max=100 #self porque la vamos a usar varias veces
+        self.cota_min=1
+
+    def input_text (self):
+        #aca hace la logica de que este entre 50 y 100, despues entre 75 y 50
+
+
+    def play (self,respuesta):
+        #
+
+
+    
+
+        
+
+
+        
     
