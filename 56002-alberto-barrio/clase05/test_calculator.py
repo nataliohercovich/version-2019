88cf8080c2a610ff712983896e303519b9ade1e6
Albertobarrio <albertobarrio2012@gmail.com>
2019-05-22 21:03:43 -0300

    un test mas

--- 56002-alberto-barrio/clase05/test_calculator.py
+++ 56002-alberto-barrio/clase05/test_calculator.py
@@ -19,6 +19,16 @@ class TestCalculator(unittest.TestCase):
         calc.input_number('=')
         self.assertEqual(calc.display(),'13')
     
+    def test_calcular_111_mas_2(self):
+        calc=Calculator()
+        calc.input_number('1')
+        calc.input_number('1')
+        calc.input_number('1')
+        calc.input_number('+')
+        calc.input_number('2')
+        calc.input_number('=')
+        self.assertEqual(calc.display(),'113')
+    
     def test_calcular_1_mas_2_mas_3(self):
         calc=Calculator()
         calc.input_number('1')
