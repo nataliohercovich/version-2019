ced6407b42f4b94d77e0de3fe2612931d6bc408f
javicercasi <jacercasii@gmail.com>
2019-06-03 19:24:27 -0300

    comit local

--- 58004-Cercasi Javier/clase06/Maquinas de cafe/cafe.py
+++ 58004-Cercasi Javier/clase06/Maquinas de cafe/cafe.py
@@ -4,7 +4,8 @@ class Maquinabasica (object):
         self.nivel_agua=1000
         self.cant_cafe=100
         self.azucar=1000
-        self.moneda=0
+        self.moneda=0                   #puede tomar valores 0 o 1
+        self.fondos=0
         self.working=False
 
     def sensor_moneda (self,mon):
@@ -15,27 +16,33 @@ class Maquinabasica (object):
 
         if mon ==1:
             if self.nivel_agua==0:
-                self.moneda= self.moneda -1
+                self.moneda= self.moneda -1     #Resta una moneda, porque la devuelve
                 return ('Disculpe,no poseo agua')
                 
-
             if self.cant_cafe==0:
                 self.moneda= self.moneda -1
                 return ('Disculpe,no poseo cafe')
                 
-
             if self.azucar==0:
                 self.moneda= self.moneda -1
                 return ('Disculpe,no poseo azucar')
               
-
             else:
                 self.working=True
-                self.moneda= self.moneda+1
+                self.fondos+=1
                 return ('Procesando, aguarde un momento')
 
+    def terminado(self):
+        if self.working==True:
+            self.working==False
+            return('Cafe hecho')
+        else:
+            return('No puedo preparar su cafe')
 
-    def CantidadAgua (self,cantidad):
+
+
+
+    def cantidadAgua (self,cantidad):
         if self.working==True:
             if cantidad > self.nivel_agua:
                 return ('Disculpe, cantidad de agua no disponible')
@@ -45,7 +52,7 @@ class Maquinabasica (object):
                 self.nivel_agua= self.nivel_agua-cantidad
                 return (str(cantidad)+'ml de agua')
 
-    def CantidadCafe (self,cafe):
+    def cantidadCafe (self,cafe):
         if self.working==True:
             if cafe > self.cant_cafe:
                 self.working=False
@@ -54,7 +61,7 @@ class Maquinabasica (object):
                 self.cant_cafe= self.cant_cafe - cafe
                 return (str(cafe)+'g. de cafe')
 
-    def CantidadAzucar (self,azucar):
+    def cantidadAzucar (self,azucar):
         if self.working==True:
             if azucar > self.azucar:
                 self.working=False
--- 58004-Cercasi Javier/clase06/Maquinas de cafe/test.py
+++ 58004-Cercasi Javier/clase06/Maquinas de cafe/test.py
@@ -9,24 +9,38 @@ class Test_CafeteraBasica(unittest.TestCase):
 
     def test_cafe_1(self):
         self.assertEqual(self.cafeB.sensor_moneda(1), 'Procesando, aguarde un momento')
-        self.assertEqual(self.cafeB.CantidadAgua(10), '10ml de agua')
-        self.assertEqual(self.cafeB.CantidadCafe(5), '5g. de cafe')
-        self.assertEqual(self.cafeB.CantidadAzucar(0), '0g. de azucar')
+        self.assertEqual(self.cafeB.cantidadAgua(10), '10ml de agua')
+        self.assertEqual(self.cafeB.cantidadCafe(5), '5g. de cafe')
+        self.assertEqual(self.cafeB.cantidadAzucar(0), '0g. de azucar')
+        self.assertEqual(self.cafeB.terminado(),'Cafe hecho')       #(Agregar () en terminado)
+
+        #Ahora restare los ingredientes de la maquina:
+        self.assertEqual(self.cafeB.nivel_agua,990)
+        self.assertEqual(self.cafeB.cant_cafe,95)
+        self.assertEqual(self.cafeB.azucar,1000)
+
 
     def test_cafe_2(self):
         self.assertEqual(self.cafeB.sensor_moneda(1), 'Procesando, aguarde un momento')
-        self.assertEqual(self.cafeB.CantidadAgua(10), '10ml de agua')
-        self.assertEqual(self.cafeB.CantidadCafe(15), '15g. de cafe')
-        self.assertEqual(self.cafeB.CantidadAzucar(3), '3g. de azucar')
+        self.assertEqual(self.cafeB.cantidadAgua(10), '10ml de agua')
+        self.assertEqual(self.cafeB.cantidadCafe(15), '15g. de cafe')
+        self.assertEqual(self.cafeB.cantidadAzucar(3), '3g. de azucar')
+        self.assertEqual(self.cafeB.terminado(),'Cafe hecho')
+        self.assertEqual(self.cafeB.nivel_agua,990)
+        self.assertEqual(self.cafeB.cant_cafe,85)
+        self.assertEqual(self.cafeB.azucar,997)
 
     
     def test_cafe_SinAgua(self):
         self.assertEqual(self.cafeB.sensor_moneda(1), 'Procesando, aguarde un momento')
-        self.assertEqual(self.cafeB.CantidadAgua(1001),'Disculpe, cantidad de agua no disponible')
+        self.assertEqual(self.cafeB.cantidadAgua(1001),'Disculpe, cantidad de agua no disponible')
+        self.assertEqual(self.cafeB.terminado(),'Cafe hecho')
+
+
     def test_cafe_SinCafe(self):
         self.assertEqual(self.cafeB.sensor_moneda(1), 'Procesando, aguarde un momento')
-        self.assertEqual(self.cafeB.CantidadAgua(10), '10ml de agua')
-        self.assertEqual(self.cafeB.CantidadCafe(101), 'Disculpe, cantidad de cafe no disponible')
+        self.assertEqual(self.cafeB.cantidadAgua(10), '10ml de agua')
+        self.assertEqual(self.cafeB.cantidadCafe(101), 'Disculpe, cantidad de cafe no disponible')
 
     def test_cafe_SinMoneda(self):
         self.assertEqual(self.cafeB.sensor_moneda(0), 'Ingrese una moneda')
